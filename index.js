5b045de7da4e5d8a2eeec60b38cf74acacc1876a
Bayan Safaee <bayan.safaee@gmail.com>
2022-07-08 00:54:20 -0700

    function to Fetch Flyover Times for ISS by coordinates

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -15,3 +15,15 @@ const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 //   }
 //   console.log('It worked! Returned coordinates: ', coordinates)
 // })
+
+const ranCoords = { latitude: '34.6937378', longitude: '135.5021651' };
+
+fetchISSFlyOverTimes(ranCoords, (error, flyOverTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:' , flyOverTimes);
+});
+
